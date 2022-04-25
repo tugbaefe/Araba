db82d99ea83e216a11cd2900c58f395013593760
tugbaefe <93380758+tugbaefe@users.noreply.github.com>
2022-04-25 14:52:10 +0300

    25042022

---
+++ src/yeniAraba/Islemler.java
@@ -0,0 +1,10 @@
+package yeniAraba;
+
+public class Islemler implements Urunler{
+
+
+    @Override
+    public void urunler(String marka, String model, int motorGucu, String renk, String yakit) {
+
+    }
+}
---
+++ src/yeniAraba/Urunler.java
@@ -0,0 +1,8 @@
+package yeniAraba;
+
+public interface Urunler {
+   static String marka="Toyota";
+
+    void urunler(String marka, String model, int motorGucu, String renk, String yakit);
+
+}
